c127517c99b7d7f83e462ccff65a4600e2c89881
kojima rikuto <kojikoji7765@icloud.com>
2021-11-22 00:38:59 +0900

    headerの作成

---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+export const Header: VFC = memo(() => {
+  return (
+    <Flex as='nav' bg='teal.500' color="gray.50" align="center" justify='space-between'>
+
+    </Flex>
+  )
+})
