b2ee31e1d01606e1ae111e12b92dc0514429f177
kojima rikuto <kojikoji7765@icloud.com>
2021-11-23 17:40:36 +0900

    ユーザー一覧画面の設定

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理画面ページです</p>
-})
+  return (
+    <Wrap p={{ base: "4", md: "10" }}>
+      <WrapItem>
+        <UserCard
+          imageUrl={"https://source.unsplash.com/random"}
+          userName={"ターボ"}
+          fullName={"リックターボ"}
+        />
+      </WrapItem>
+    </Wrap>
+  );
+});
